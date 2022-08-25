a159506b807a33b89e0d586332d262d2488368f0
Austin Chao <austinschao@gmail.com>
2022-08-25 13:55:46 -0500

    Added leave-room feature

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ const io = new Server(3001, {
 
 io.on("connection", socket => {
   console.log(socket.id);
-  socket.on("joinRoom", (username, room) => {
+  socket.on("join-room", (username, room) => {
     socket.join(room);
-    socket.to(room).emit("receiveMessage", `${username} has joined room: ${room}.`);
-
+    socket.emit("receive-message", `${username} has joined room: ${room}.`);
+    socket.to(room).emit("receive-message", `${username} has joined room: ${room}.`);
+  });
+  socket.on("leave-room", (username, room) => {
+    console.log(room, "room");
+    socket.to(room).emit("receive-message", `${username} has left room: ${room}.`);
+    socket.leave(room);
   });
   socket.on("send-message", (username, message, room) => {
-    socket.to(room).emit("receiveMessage", `${username}: ${message}`);
+    socket.to(room).emit("receive-message", `${username}: ${message}`);
   });
 });;
